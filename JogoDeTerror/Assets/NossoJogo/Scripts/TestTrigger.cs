f88c92b651aeed0da2a95da17948c31d0f5d7c02
SolarOracle <antoniopsmasetti@gmail.com>
2025-11-15 00:33:26 -0300

    Progresso Trigger

--- JogoDeTerror/Assets/NossoJogo/Scripts/TestTrigger.cs
+++ JogoDeTerror/Assets/NossoJogo/Scripts/TestTrigger.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class TestTrigger : MonoBehaviour
 {
+    public GameObject testObject;
+
     private void OnTriggerEnter(Collider other)
     {
         print("Trigger entered!");
+        testObject.GetComponent<MeshRenderer>().enabled = true;
     }
 }
