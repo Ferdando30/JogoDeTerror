e9b8d10092f170dce5fd6180f74f2b7c28b5df24
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2025-11-20 12:58:22 -0300

    banheiro e cofre
    dei uma mexida no banheiro da casa e terminei o codigo do cofre

--- JogoDeTerror/Assets/NossoJogo/Scripts/SafeCode.cs
+++ JogoDeTerror/Assets/NossoJogo/Scripts/SafeCode.cs
@@ -20,6 +20,8 @@ public class SafeCode : MonoBehaviour
     string codeTextValue;
 
     public GameObject PortaCofre;
+    public bool closed;
+    public GameObject book;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -37,6 +39,7 @@ public class SafeCode : MonoBehaviour
         ZeroBtn.gameObject.SetActive(false);
 
         PortaCofre.transform.rotation = Quaternion.Euler(-90f, 0f, 0f);
+        closed = true;
     }
 
     // Update is called once per frame
@@ -60,6 +63,7 @@ public class SafeCode : MonoBehaviour
             ZeroBtn.gameObject.SetActive(false);
 
             PortaCofre.transform.rotation = Quaternion.Euler(-90f, 0f, -90f);
+            closed = false;
 
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
@@ -81,24 +85,36 @@ public class SafeCode : MonoBehaviour
     {
         if (hit.tag == "Player")
         {
-            if (Input.GetKey(KeyCode.E))
+            if(closed == true)
             {
-                Panel.SetActive(true);
-                codeText.enabled = true;
-                OneBtn.gameObject.SetActive(true);
-                TwoBtn.gameObject.SetActive(true);
-                ThreeBtn.gameObject.SetActive(true);
-                FourBtn.gameObject.SetActive(true);
-                FiveBtn.gameObject.SetActive(true);
-                SixBtn.gameObject.SetActive(true);
-                SevenBtn.gameObject.SetActive(true);
-                EightBtn.gameObject.SetActive(true);
-                NineBtn.gameObject.SetActive(true);
-                ZeroBtn.gameObject.SetActive(true);
+                if (Input.GetKey(KeyCode.E))
+                {
+                    Panel.SetActive(true);
+                    codeText.enabled = true;
+                    OneBtn.gameObject.SetActive(true);
+                    TwoBtn.gameObject.SetActive(true);
+                    ThreeBtn.gameObject.SetActive(true);
+                    FourBtn.gameObject.SetActive(true);
+                    FiveBtn.gameObject.SetActive(true);
+                    SixBtn.gameObject.SetActive(true);
+                    SevenBtn.gameObject.SetActive(true);
+                    EightBtn.gameObject.SetActive(true);
+                    NineBtn.gameObject.SetActive(true);
+                    ZeroBtn.gameObject.SetActive(true);
+                    
 
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-                Time.timeScale = 0;
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                    Time.timeScale = 0;
+                }
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.E))
+                {
+                    Destroy(book);
+                }
+                    
             }
         }
     }
