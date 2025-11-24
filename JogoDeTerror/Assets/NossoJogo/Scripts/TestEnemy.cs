b6cbdc641ef197ad679b415fae17a7eec5b50bd8
SolarOracle <antoniopsmasetti@gmail.com>
2025-11-23 21:47:09 -0300

    Rigidbody no inimigo. Estado de Chase implementado de novo.

--- JogoDeTerror/Assets/NossoJogo/Scripts/TestEnemy.cs
+++ JogoDeTerror/Assets/NossoJogo/Scripts/TestEnemy.cs
@@ -6,14 +6,21 @@ public class TestEnemy : MonoBehaviour
     public Transform[] patrolPoints;
     public float chaseSpeed = 4f;
     public float patrolSpeed = 2f;
+    public float viewDistance = 10f;
+    public float viewAngle = 45f;
+    public LayerMask visionMask;
 
     private NavMeshAgent agent;
     private int currentPoint = 0;
-
-    public enum EnemyState { Patrol, Chase, Return }
-    public EnemyState state = EnemyState.Patrol;
-
     public Transform player;
+
+    public enum EnemyState
+    {
+        Patrol,
+        Chase,
+        Return
+    }
+    public EnemyState state = EnemyState.Patrol;
 
     void Start()
     {
@@ -21,6 +28,7 @@ public class TestEnemy : MonoBehaviour
         agent.speed = patrolSpeed;
         agent.SetDestination(patrolPoints[currentPoint].position);
     }
+
 
     void Update()
     {
@@ -48,6 +56,7 @@ public class TestEnemy : MonoBehaviour
         {
             currentPoint = (currentPoint + 1) % patrolPoints.Length;
             agent.SetDestination(patrolPoints[currentPoint].position);
+            if (CanSeePlayer() ) StartChase();
         }
     }
 
@@ -73,6 +82,39 @@ public class TestEnemy : MonoBehaviour
 
     bool CanSeePlayer()
     {
+        if (player == null) return false;
+
+        Vector3 dirToPlayer = (player.position - transform.position).normalized;
+        Vector3 origin = transform.position + Vector3.up * 0.5f;
+
+        if (Vector3.Angle(transform.forward, dirToPlayer) > viewAngle)
+            return false;
+
+        if (Vector3.Distance(transform.position, player.position) > viewDistance)
+            return false;
+
+        if (Physics.Raycast(origin, dirToPlayer, out RaycastHit hit, viewDistance, visionMask))
+        {
+            return hit.transform == player;
+        }
+
         return false;
+    }
+    void OnDrawGizmos()
+    {
+#if UNITY_EDITOR
+        if (!UnityEditor.EditorApplication.isPlaying)
+        {
+            // Draw Gizmos ONLY in Editor Mode
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, viewDistance);
+
+            Vector3 leftBoundary = Quaternion.Euler(0, -viewAngle, 0) * transform.forward * viewDistance;
+            Vector3 rightBoundary = Quaternion.Euler(0, viewAngle, 0) * transform.forward * viewDistance;
+
+            Gizmos.DrawLine(transform.position, transform.position + leftBoundary);
+            Gizmos.DrawLine(transform.position, transform.position + rightBoundary);
+        }
+#endif
     }
 }
